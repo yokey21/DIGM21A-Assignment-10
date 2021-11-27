d8edce39edb4fbc730c234ec3c0dd99619a2581f
Adam Byron <abyron939@gmail.com>
2021-11-26 21:24:00 -0800

    Created Repo

---
+++ main.js
@@ -0,0 +1,46 @@
+// declare a variable using the keyword "let" and the identifier aka the name of "cats" and assign the value of 22
+
+
+
+
+
+
+
+
+// declare a variable using the identifier aka the name of "dogs" and assign the value of 13
+
+
+
+
+
+
+
+
+
+// declare a variable using the identifier aka the name of "addingVariables" and assign the expression of "cats + dogs"
+
+
+
+
+
+
+
+
+
+// declare a variable using the identifier aka the name of "addingStrings" and concat 'javascript is ' + 'really awesome!' 
+
+
+
+
+
+
+
+
+
+// declare an array using the identifier aka the name of "myArray" and add these three items into it: 'Coding is awesome', 500, false
+
+
+// resassign myArray[1] to 250
+
+
+// creat your own variable and write a random string/message in it
