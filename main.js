3612c012bc172ed53b0b8554d0f8daea1f817219
Adam Byron <abyron939@gmail.com>
2021-11-27 21:09:34 -0800

    Completed Assignment

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // declare a variable using the keyword "let" and the identifier aka the name of "cats" and assign the value of 22
 
+let cats = 22
 
 
 
@@ -8,7 +9,7 @@
 
 
 // declare a variable using the identifier aka the name of "dogs" and assign the value of 13
-
+let dogs = 13
 
 
 
@@ -18,7 +19,7 @@
 
 
 // declare a variable using the identifier aka the name of "addingVariables" and assign the expression of "cats + dogs"
-
+let addingVariables = cats + dogs
 
 
 
@@ -28,7 +29,8 @@
 
 
 // declare a variable using the identifier aka the name of "addingStrings" and concat 'javascript is ' + 'really awesome!' 
-
+let addingStrings = "javascript is " + "really awesome!"
+//console.log(addingStrings)
 
 
 
@@ -38,9 +40,10 @@
 
 
 // declare an array using the identifier aka the name of "myArray" and add these three items into it: 'Coding is awesome', 500, false
-
+let myArray = ['Coding is awesome', 500, false]
 
 // resassign myArray[1] to 250
+myArray[1] = 250
 
-
-// creat your own variable and write a random string/message in it
+// create your own variable and write a random string/message in it
+let myVariable = "Hello World!"
